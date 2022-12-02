7b1edada667ec2de00097467253ca42d2b08d44b
Sanskar Parihar <200489011@student.georgianc.on.ca>
2022-12-01 22:14:18 -0500

    assignment-3

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const products = require('./routes/products');
+const auth = require('./routes/auth');
+const passport = require('passport')
+const session = require('express-session')
+
+
 
 var app = express();
 
@@ -14,6 +19,44 @@ var app = express();
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+
+// passport config for auth
+app.use(session({
+  secret: process.env.PASSPORT_SECRET,
+  resave: true,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+const User = require('./models/user')
+passport.use(User.createStrategy())
+
+// let passport read/write user data to/from session vars
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+
+
+// google auth config
+const google = require('passport-google-oauth20').Strategy
+passport.use(new google({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: process.env.GOOGLE_CALLBACK_URL
+},
+  (accessToken, refreshToken, profile, done) => {
+    User.findOrCreate({ oauthId: profile.id }, {
+      username: profile.displayName,
+      oauthProvider: 'Google',
+      created: Date.now()
+    }, (err, user) => {
+      return done(err, user)
+    })
+  }
+))
+
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL)
@@ -37,6 +80,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', products)
+app.use('/auth', auth)
+
+// hbs helper function to pre-select correct dropdown option
+const hbs = require('hbs')
+
+hbs.registerHelper('selectCorrectOption', (currentVal, selectedVal) => {
+  // if values match, append ' selected' to this option tag
+  let selectedProperty = ''
+  if (currentVal === selectedVal) {
+    selectedProperty = ' selected'
+  }
+
+  return new hbs.SafeString(`<option${selectedProperty}>${currentVal}</option>`)
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/products.js
+++ routes/products.js
@@ -1,33 +1,54 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('passport')
+const globals = require('./globalFunctions')
 
-// import Region model
+
 const Product = require('../models/product')
+
+function isAuthenticated(req, res, next){
+    if (req.isAuthenticated()){
+      return next()
+    }
+    res.redirect('/auth/login')
+  }
 
 // GET: /products => show list of products
 router.get('/', (req, res) => {
-    // query the model to fetch & pass the region data to the view
+    Product.find((err, products) => {
+        if (err) {
+            console.log(err);
+        } 
+        else {
+            res.render('products/index', {
+                title: 'Products', 
+                products: products,
+                user: req.user
+            })
+        }
+    })
+})
+
+// GET: /products/create => display blank form
+router.get('/create', isAuthenticated, (req, res) => {
+    
     Product.find((err, products) => {
         if (err) {
             console.log(err)
         }
         else {
-            res.render('products/index', { 
-                title: 'Products',
-                products: products
+            res.render('products/create', { 
+                title: 'Add Product',
+                products: products,
+                user: req.user
             })
         }
-    })  
-})
-
-// GET: /products/create => show blank product form
-router.get('/create', (req, res) => {
-    res.render('products/create', { title: 'Add Product'})
+    }).sort('name')   
 })
 
 // POST: /products/create => process form submission
-router.post('/create', (req, res) => {
-    // create a new Region document from the fields in the form post
+router.post('/create', isAuthenticated, (req, res) => {
+    
     Product.create(req.body, (err, newProduct) => {
         if (err) {
             console.log(err)
@@ -38,8 +59,8 @@ router.post('/create', (req, res) => {
     })
 })
 
-// GET: /employers/delete/abc123 => remove selected Employer document
-router.get('/delete/:_id', (req, res) => {
+// GET: /products/delete/abc123 => remove selected Productdocument
+router.get('/delete/:_id', isAuthenticated, (req, res) => {
     Product.remove({ _id: req.params._id }, (err) => {
         if (err) {
             console.log(err)
@@ -50,15 +71,15 @@ router.get('/delete/:_id', (req, res) => {
     })
 })
 
-// GET: /employers/edit/abc123 => display populated form for editing
-router.get('/edit/:_id', (req, res) => {
-    // get regions for Form dropdown
+// GET: /products/edit/abc123 => display populated form for editing
+router.get('/edit/:_id', isAuthenticated, (req, res) => {
+    
     Product.find((err, products) => {
         if (err) {
             console.log(err)
         }
         else {
-            // fetch selected Employer for display
+            // fetch selected Product for display
             Product.findById(req.params._id, (err, product) => {
                 if (err) {
                     console.log(err)
@@ -67,6 +88,7 @@ router.get('/edit/:_id', (req, res) => {
                     res.render('products/edit', { 
                         title: 'Product Details',
                         products: products,
+                        user: req.user
                         
                     })
                 }
@@ -75,8 +97,8 @@ router.get('/edit/:_id', (req, res) => {
     }).sort('name')   
 })
 
-// POST: /employers/edit/abc123 => update the db for the selected doc
-router.post('/edit/:_id', (req, res) => {
+// POST: /products/edit/abc123 => update the db for the selected doc
+router.post('/edit/:_id', isAuthenticated, (req, res) => {
     Product.findByIdAndUpdate({ _id: req.params._id }, req.body, null, (err, product) => {
         if (err) {
             console.log(err)
